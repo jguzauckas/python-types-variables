1c4f71b1e810cb69807cbfdd76d8dbcc332dd999
jguzauckas <75325154+jguzauckas@users.noreply.github.com>
2022-09-12 08:37:39 -0400

    Added and prepared homework.py

---
+++ homework.py
@@ -0,0 +1,20 @@
+#This Python file should fulfill the requirements listed in comments throughout this template
+
+#Define 3 number variables, where you have at least one integer and at least one float
+#You choose the values
+
+
+#Show that you can perform each kind of operation using some combination of your three
+#number variables from above (+, -, *, /, //, %, )
+
+#Define 2 boolean variables, you choose the values
+
+
+#Show that you can perform each kind of operation using your two boolean values
+#(integer cast, not, and, or)
+
+
+#Define two string variables, you choose the values
+
+
+#Show that you can perform each kind of operation using your string values (len, index, splice)
