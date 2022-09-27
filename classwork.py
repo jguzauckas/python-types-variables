8b14111af93bc5e0ca291911bc03ef8e975b1e15
jguzauckas <75325154+jguzauckas@users.noreply.github.com>
2022-09-27 18:06:00 -0400

    Fixed error and updated formatting

--- classwork.py
+++ classwork.py
@@ -1,20 +1,20 @@
-#This Python file needs to be completed so that the output matches what is written
-#in classwork.txt
+# This Python file needs to be completed so that the output matches what is written
+# in classwork.txt
 
-#Here is a sample:
+# Here is a sample:
 a = 5
 b = 10
-print(a + b) #Note that this just prints the sum, that's okay!
+print(a + b)  # Note that this just prints the sum, that's okay!
 
-#First, define variables with the values 16, 35, 13.2, 23.6, True, False, "Hello", "World"
-
-
-#Next lets do some operations. Using print, your variables, and your operations, do
-#16 + 35, 35 - 13.2, 13.2 * 23.6, 23.6 / 16, 35 // 16, 23.6 % 16, 16 ** 13.2
+# First, define variables with the values 16, 35, 13.2, 23.6, True, False, "Hello", "World"
 
 
-#not True, True and False, True or False
+# Next lets do some operations. Using print, your variables, and your operations, do
+# 16 + 35, 35 - 13.2, 13.2 * 23.6, 23.6 / 16, 35 // 16, 23.6 % 16, 16 ** 13.2
 
 
-#The length of "Hello", the third character of "World", from beginning to 4 of "Hello"
-#from 2 to end of "World", from 2 to 5 of "Hello"
+# not True, True and False, True or False
+
+
+# The length of "Hello", the third character of "World", from beginning to 4 of "Hello"
+# from 2 to end of "World", from 2 to 5 of "Hello"
--- homework.py
+++ homework.py
@@ -1,20 +1,20 @@
-#This Python file should fulfill the requirements listed in comments throughout this template
+# This Python file should fulfill the requirements listed in comments throughout this template
 
-#Define 3 number variables, where you have at least one integer and at least one float
-#You choose the values
-
-
-#Show that you can perform each kind of operation using some combination of your three
-#number variables from above (+, -, *, /, //, %, )
-
-#Define 2 boolean variables, you choose the values
+# Define 3 number variables, where you have at least one integer and at least one float
+# You choose the values
 
 
-#Show that you can perform each kind of operation using your two boolean values
-#(integer cast, not, and, or)
+# Show that you can perform each kind of operation using some combination of your three
+# number variables from above (+, -, *, /, //, %, )
+
+# Define 2 boolean variables, you choose the values
 
 
-#Define two string variables, you choose the values
+# Show that you can perform each kind of operation using your two boolean values
+# (integer cast, not, and, or)
 
 
-#Show that you can perform each kind of operation using your string values (len, index, splice)
+# Define two string variables, you choose the values
+
+
+# Show that you can perform each kind of operation using your string values (len, index, splice)
